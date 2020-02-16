badd1b4a8af80d132126641fb70a05c79b8670b6
Student 04 <ACE04@utorcsi.utoronto.ca>
2020-02-16 13:11:54 -0500

    added timeline

--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
         // Transition effect for navbar
         $(window).scroll(function() {
           // checks if window is scrolled more than 200px, adds/removes solid class
-          if($(this).scrollTop() > 200) {
+          if($(this).scrollTop() > 100) {
               $('.navbar').addClass('solid');
           } else {
               $('.navbar').removeClass('solid');
